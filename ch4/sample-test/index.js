dd11ad8ac22b4584fb057eb9628cff42780f1038
hugo <werwer748@naver.com>
2023-08-11 18:03:10 +0900

    pnpm으로...? 모노레포 같은것도 공부해봐야할듯

---
+++ ch4/sample-test/index.js
@@ -0,0 +1,9 @@
+const calc = require("sample-package");
+
+const a = 23;
+const b = 7;
+
+console.log("a + b = ", calc.add(a, b));
+console.log("a - b = ", calc.sub(a, b));
+console.log("a * b = ", calc.multi(a, b));
+console.log("a / b = ", calc.div(a, b));
